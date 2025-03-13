88a412d1d021a2067263359177b407d28596c071
unknown <Niki@DESKTOP-1PHQI24>
2025-03-13 14:05:27 +0100

    Reapply "Dodanie nowych klas oraz interfejsów"
    This reverts commit 580ca9bb5dfe939c8eba276d870489114b9fdf6a.

---
+++ ProcesowanieZamowienia_PG/IPlatnosci.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesowanieZamowienia_PG
+{
+    internal interface IPlatnosci
+    {
+        public void Przetworz();
+    }
+}
